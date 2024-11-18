558ed4551b828c45ac394031b9992835a96c8543
ChesterK01 <tuandnse173227@fpt.edu.vn>
2024-11-18 16:42:16 +0700

    update

--- db/setup.sql
+++ db/setup.sql
@@ -1,10 +1,10 @@
 -- Tạo database
-CREATE DATABASE IF NOT EXISTS library;
+CREATE DATABASE IF NOT EXISTS hello_db;
 
 -- Sử dụng database
-USE library;
+USE hello_db;
 
--- Tạo bảng hello_world
+-- Tạo bảng greetings
 CREATE TABLE IF NOT EXISTS hello_world (
     id INT AUTO_INCREMENT PRIMARY KEY,
     message VARCHAR(255) NOT NULL
